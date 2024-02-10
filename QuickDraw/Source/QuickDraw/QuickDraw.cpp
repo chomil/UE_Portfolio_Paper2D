b6bdbe113170e03e8ad300789861ab7921c3b811
kimyounghoon <chomil96@naver.com>
2024-02-10 22:18:36 +0900

    Unreal 2D Project Start

---
+++ QuickDraw/Source/QuickDraw/QuickDraw.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "QuickDraw.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuickDraw, "QuickDraw" );
