1d5bd4193a925bfcd20efa6557fa9f7319dbf4b4
kimyounghoon <chomil96@naver.com>
2024-03-02 21:50:41 +0900

    Runner Project Start

---
+++ MashRunner/Source/MashRunner/MashRunner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MashRunner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MashRunner, "MashRunner" );
